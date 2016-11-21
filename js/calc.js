a426d18cfea0d3886e6f448186fd128b1c6e343f
river0825 <riverdm@gmail.com>
2016-11-21 18:21:08 +0800

    Update calc.js

--- js/calc.js
+++ js/calc.js
@@ -8,59 +8,57 @@
 
 $(document).ready(function() {
   $('.collapsible').collapsible();
-});
+  var vm = new Vue({
+    el: '#vue-instance',
+    data: {
+      max: null,
+      static: null,
+      zone0: 0,
+      zone1: 0,
+      zone2: 0,
+      zone3: 0,
+      zone4: 0,
+      zone5: 0,
+      config: {
+        z0: {
+          rate: 0.59,
+          caption: "第0級強度：D 熱身與技術區"
+        },
 
-var vm = new Vue({
-  el: '#vue-instance',
-  data: {
-    max: null,
-    static: null,
-    zone0: 0,
-    zone1: 0,
-    zone2: 0,
-    zone3: 0,
-    zone4: 0,
-    zone5: 0,
-    config: {
-      z0: {
-        rate: 0.59,
-        caption: "第0級強度：D 熱身與技術區"
-      },
-
-      z1: {
-        rate: 0.65,
-        caption: "第1級強度：E 有氧耐力區"
-      },
-      z2: {
-        rate: 0.8,
-        caption: "第2級強度：M 馬拉松配速區"
-      },
-      z3: {
-        rate: 0.88,
-        caption: "第3級強度：T 乳酸耐力區"
-      },
-      z4: {
-        rate: 0.92,
-        caption: "第4級強度：A 無氧耐力區"
-      },
-      z5: {
-        rate: 0.975,
-        caption: "第5級強度：I 最大耗氧區"
+        z1: {
+          rate: 0.65,
+          caption: "第1級強度：E 有氧耐力區"
+        },
+        z2: {
+          rate: 0.8,
+          caption: "第2級強度：M 馬拉松配速區"
+        },
+        z3: {
+          rate: 0.88,
+          caption: "第3級強度：T 乳酸耐力區"
+        },
+        z4: {
+          rate: 0.92,
+          caption: "第4級強度：A 無氧耐力區"
+        },
+        z5: {
+          rate: 0.975,
+          caption: "第5級強度：I 最大耗氧區"
+        }
       }
-    }
-  },
-  methods: {
-    setAlg: function(alg) {
-      this.alg = alg;
-      this.calculate();
     },
-    calculate: function() {
-      switch (this.alg) {
-        case 'mhr':
-          this.setHeartRate()
-          break;
-        case 'hhr':
-          this.setHHRate();
+    methods: {
+      setAlg: function(alg) {
+        this.alg = alg;
+        this.calculate();
+      },
+      calculate: function() {
+        switch (this.alg) {
+          case 'mhr':
+            this.setHeartRate()
+            break;
+          case 'hhr':
+            this.setHHRate();
           break
         default:
           this.setHeartRate()
@@ -84,4 +82,5 @@ var vm = new Vue({
       this.zone5 = Math.round((this.max - this.static) * this.config.z5.rate) + this.static * 1;
     }
   }
+});
 });
